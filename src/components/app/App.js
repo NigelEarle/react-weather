d615f03902cc8f7a3b33bd5c83ec6e0a87c5b384
nwre87 <nwre87@gmail.com>
2017-05-31 21:43:52 -1000

    adjust folder structure

---
+++ src/components/app/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class AppComponent extends Component {
+  render() {
+    return (
+      <div className="appContainer">
+        <h1>React Weather</h1>
+      </div>
+    );
+  }
+}
+
+export default AppComponent;
